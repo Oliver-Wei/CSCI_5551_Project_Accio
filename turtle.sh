65a4ab4456257923ce61727e9ed0b8b4376aaea8
Oliver-Wei <115010240@link.cuhk.edu.cn>
2018-12-17 23:20:33 -0600

    Delete turtle.sh

--- turtle.sh
+++
@@ -1,5 +0,0 @@
-virtualenv ENV
-source /home/turtlebot/ENV/bin/activate
-pip install --upgrade google-cloud-speech
-pip install pyserial
-export GOOGLE_APPLICATION_CREDENTIALS="/home/turtlebot/Downloads/CSCI5551-speech-recognition-0f428c026c14.json"
